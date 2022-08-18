96f86963c931558fb6f4391244d1afe7650063d3
dangjinghao <dangjinghaoemail@163.com>
2022-08-19 00:44:26 +0800

    修改中间件注册流程 取消在反代被用户取消时的错误日志

--- src/Library/ProxyLibrary.go
+++ src/Library/ProxyLibrary.go
@@ -2,6 +2,8 @@ package Library
 
 import (
 	"TheresaProxyV2/src/Register"
+	"context"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"net/http/httputil"
@@ -14,13 +16,16 @@ func CookieProxy(c *gin.Context) (err error) {
 		c.String(http.StatusBadRequest, "错误的cookie")
 		return nil
 	}
-
 	if Register.ProxySiteCore[cookieProxyDomain] == nil {
 		c.String(http.StatusBadRequest, "不允许访问的域名")
 		return nil
 	}
+	if Register.MiddlewareCore[cookieProxyDomain] != nil {
+		(*Register.MiddlewareCore[cookieProxyDomain])(c)
+	}
 	proxyTargetUrl := c.Request.URL
 	proxyTargetUrl.Host = cookieProxyDomain
+
 	proxyTargetUrl.Scheme = Register.ProxySiteCore[cookieProxyDomain].Scheme
 	if Register.ProxySiteCore[cookieProxyDomain].RequestModify != nil {
 		Register.ProxySiteCore[cookieProxyDomain].RequestModify(c.Request)
@@ -33,7 +38,10 @@ func CookieProxy(c *gin.Context) (err error) {
 	}
 
 	proxy := &httputil.ReverseProxy{
-		Director:       director,
+		Director: director,
+		ErrorHandler: func(http.ResponseWriter, *http.Request, error) {
+			// handle error
+		},
 		ModifyResponse: modifyResponseMain(proxyTargetUrl),
 	}
 	proxy.ServeHTTP(c.Writer, c.Request)
@@ -43,6 +51,9 @@ func CookieProxy(c *gin.Context) (err error) {
 // 通过param参数来获取domain
 func ParamProxy(proxyDomain string) func(c *gin.Context) {
 	return func(c *gin.Context) {
+		if Register.MiddlewareCore[proxyDomain] != nil {
+			(*Register.MiddlewareCore[proxyDomain])(c)
+		}
 
 		proxyTargetUrl := c.Request.URL
 		proxyTargetUrl.Path = proxyTargetUrl.Path[1+len(proxyDomain):]
@@ -60,6 +71,7 @@ func ParamProxy(proxyDomain string) func(c *gin.Context) {
 
 		proxy := &httputil.ReverseProxy{
 			Director:       director,
+			ErrorHandler:   noContextCancelErrors,
 			ModifyResponse: modifyResponseMain(proxyTargetUrl),
 		}
 
@@ -67,4 +79,13 @@ func ParamProxy(proxyDomain string) func(c *gin.Context) {
 		proxy.ServeHTTP(c.Writer, c.Request)
 
 	}
+}
+
+// https://github.com/golang/go/issues/20071#issuecomment-926644055
+func noContextCancelErrors(rw http.ResponseWriter, req *http.Request, err error) {
+	if err != context.Canceled {
+
+		fmt.Errorf("http: proxy error: %v", err)
+	}
+	rw.WriteHeader(http.StatusBadGateway)
 }
--- src/main.go
+++ src/main.go
@@ -3,19 +3,13 @@ package main
 import (
 	_ "TheresaProxyV2/plugins"
 	"TheresaProxyV2/src/Frame"
-	"TheresaProxyV2/src/Register"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
-	for _, v := range Register.MiddlewareCore {
-		r.Use(*v)
-	}
-
-	r.Use()
 	r.Any("/*url", Frame.TinyRouteHandler)
-
 	r.Run()
 
 }
